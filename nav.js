a3d524ff34bc7d9dee552d63d297ba16faf13940
Styrbjörn Nordberg <styris.n@gmail.com>
2023-02-22 14:11:24 +0100

    changed nav bar transition threshold

--- nav.js
+++ nav.js
@@ -1,16 +1,16 @@
-const navLinks = document.querySelectorAll(".nav-item a");
-const pages = document.querySelectorAll(".page");
+const navLinks = document.querySelectorAll('.nav-item a');
+const pages = document.querySelectorAll('.page');
 
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
       navLinks[entry.target.dataset.index].classList.toggle(
-        "active-nav",
+        'active-nav',
         entry.isIntersecting
       );
     });
   },
-  { threshold: 0.51 }
+  { threshold: 0.2 }
 );
 
 pages.forEach((page) => {
