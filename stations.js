e385cde7dba3e105ed7e7d14308f09737a1e3191
jaken92 <jaaakeeen@gmail.com>
2023-02-24 12:14:10 +0100

    updated function in stations.js

--- stations.js
+++ stations.js
@@ -30,116 +30,124 @@ fetch(
     theLoop("infoRight", line2, secondHalf);
   });
 
-    //function for looping out the data and creating elements
-    function theLoop(infoDivName, container, data) {
-      data.forEach((station) => {
-        
-        // Counter for looping through image array.
-        let i = 0;
+//function for looping out the data and creating elements
+function theLoop(infoDivName, container, data) {
+  data.forEach((station) => {
+    // Counter for looping through image array.
+    let i = 0;
 
-        let img = new Image();
-        img.classList.add("station-image");
-        img.src = svgImages[i];
+    let img = new Image();
+    img.classList.add("station-image");
+    img.src = svgImages[i];
 
-        const stationCircle = document.createElement("div");
-        const stationTitle = document.createElement("p");
-        stationTitle.textContent = station.Description;
-        stationTitle.classList.add("station-name");
-        stationCircle.classList.add("stationCircle");
+    const stationCircle = document.createElement("div");
+    const stationTitle = document.createElement("p");
+    stationTitle.textContent = station.Description;
+    stationTitle.classList.add("station-name");
+    stationCircle.classList.add("stationCircle");
 
-        stationCircle.append(img);
+    stationCircle.append(img);
 
-        i++;
+    i++;
 
-        if (i == svgImages.length) {
-          i = 0;
-        }
+    if (i == svgImages.length) {
+      i = 0;
+    }
 
-        const wrapperDiv = document.createElement("div");
-        wrapperDiv.classList.add("wrapperDiv");
-        container.append(wrapperDiv);
-        const infoDivName = document.createElement("div");
-        wrapperDiv.append(stationCircle);
-        wrapperDiv.append(infoDivName);
-        wrapperDiv.append(stationTitle);
-        const infoStationName = document.createElement("p");
-        infoStationName.textContent = station.Description;
-        infoStationName.classList.add("info-box-headline");
-        infoDivName.classList.add("hidden");
-        infoDivName.append(infoStationName);
+    const wrapperDiv = document.createElement("div");
+    wrapperDiv.classList.add("wrapperDiv");
+    container.append(wrapperDiv);
+    const infoDivName = document.createElement("div");
+    wrapperDiv.append(stationCircle);
+    wrapperDiv.append(infoDivName);
+    wrapperDiv.append(stationTitle);
+    const infoStationName = document.createElement("p");
+    infoStationName.textContent = station.Description;
+    infoStationName.classList.add("info-box-headline");
+    infoDivName.classList.add("hidden");
+    infoDivName.append(infoStationName);
 
-        // Create info elements
-        const measurement = document.createElement("p");
-        measurement.classList.add("measurement");
+    // Create info elements
+    const measurement = document.createElement("p");
+    measurement.classList.add("measurement");
 
-        const dg = document.createElement("p");
+    const dg = document.createElement("p");
 
-        const sg = document.createElement("p");
+    const sg = document.createElement("p");
 
-        const current = document.createElement("p");
+    const current = document.createElement("p");
 
-        console.log(station);
+    console.log(station);
 
-         //Append infoDivName elements
-         const infoBoxArrow = document.createElement("div");
-         const infoBoxClose = document.createElement("img");
-         infoBoxArrow.classList.add("info-box-arrow");
-         infoBoxClose.classList.add("close-info-button");
-         infoBoxClose.src = "images/close-button.svg";
-         infoDivName.append(infoBoxArrow);
-         infoDivName.append(infoBoxClose);
-         infoDivName.append(infoStationName);
+    //Append infoDivName elements
+    const infoBoxArrow = document.createElement("div");
+    const infoBoxClose = document.createElement("img");
+    infoBoxArrow.classList.add("info-box-arrow");
+    infoBoxClose.classList.add("close-info-button");
+    infoBoxClose.src = "images/close-button.svg";
+    infoDivName.append(infoBoxArrow);
+    infoDivName.append(infoBoxClose);
+    infoDivName.append(infoStationName);
 
-         //Append info elements
-         infoDivName.append(measurement);
-         infoDivName.append(current);
+    //Append info elements
+    infoDivName.append(measurement);
+    infoDivName.append(current);
 
-        // Print out info about station
-        measurement.textContent = `Current water level is at: ${station.MeasureParameters[0].CurrentValue}m`;
-        dg.textContent = `Upper limit is at: ${station.DG}cm`;
-        sg.textContent = `Lower limit is at: ${station.SG}cm`;
+    // Print out info about station
+    measurement.textContent = `Current water level is at: ${station.MeasureParameters[0].CurrentValue}m`;
+    dg.textContent = `Upper limit is at: ${station.DG}cm`;
+    sg.textContent = `Lower limit is at: ${station.SG}cm`;
 
-        station.MeasureParameters.forEach((measureObject) => {
-          if (measureObject.Code.includes("Flow")) {
-            const flow = document.createElement("p");
-            flow.textContent = `Flow is at: ${measureObject.CurrentValue}m3/s`;
-            infoDivName.append(flow);
-          }
-        });
+    station.MeasureParameters.forEach((measureObject) => {
+      if (measureObject.Code.includes("Flow")) {
+        const flow = document.createElement("p");
+        flow.textContent = `Flow is at: ${measureObject.CurrentValue}m3/s`;
+        infoDivName.append(flow);
+      }
+    });
 
-        if ("DG" && "SG" in station) {
-          infoDivName.append(dg);
-          infoDivName.append(sg);
-        }
-
-        //Append info elements
+    station.MeasureParameters.forEach((measureObject) => {
+      if ("CurrentValue" in measureObject) {
         infoDivName.append(measurement);
         infoDivName.append(current);
+      } else {
+        measurement.textContent = `No current water level available`;
+      }
+    });
 
-        stationCircle.addEventListener("click", () => {
-          infoDivName.classList.toggle("active");
-        });
+    if ("DG" && "SG" in station) {
+      infoDivName.append(dg);
+      infoDivName.append(sg);
+    }
 
-        infoDivName.addEventListener("click", () => {
-          infoDivName.classList.toggle("active");
-        });
-        // Adds the "animate" class to the element which starts the animation.
-        activateAnimation();
-        function activateAnimation() {
-          stationCircle.addEventListener("click", () => {
-            stationCircle.classList.add("animate");
-            removeAnimation();
-          });
-        }
+    //Append info elements
+    infoDivName.append(measurement);
+    infoDivName.append(current);
 
-        // Removes the "animate" class from the element right after it's run (750ms).
-        function removeAnimation() {
-          setTimeout(() => {
-            stationCircle.classList.remove("animate");
-          }, 750);
-        }
+    stationCircle.addEventListener("click", () => {
+      infoDivName.classList.toggle("active");
+    });
+
+    infoDivName.addEventListener("click", () => {
+      infoDivName.classList.toggle("active");
+    });
+    // Adds the "animate" class to the element which starts the animation.
+    activateAnimation();
+    function activateAnimation() {
+      stationCircle.addEventListener("click", () => {
+        stationCircle.classList.add("animate");
+        removeAnimation();
       });
     }
+
+    // Removes the "animate" class from the element right after it's run (750ms).
+    function removeAnimation() {
+      setTimeout(() => {
+        stationCircle.classList.remove("animate");
+      }, 750);
+    }
+  });
+}
 
 console.log("funka");
 
